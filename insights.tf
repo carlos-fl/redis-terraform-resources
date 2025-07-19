afffddcb0300d5a670f0ad2efdae80129afb12e4
carlos <carlosftru@gmail.com>
2025-07-18 23:46:08 -0600

    added insight resource

---
+++ insights.tf
@@ -0,0 +1,15 @@
+resource "azurerm_log_analytics_workspace" "workspace" {
+  name                = "workspace-api"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "insights" {
+  name                = "api-appinsights"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.workspace.id
+  application_type    = "web"
+}
